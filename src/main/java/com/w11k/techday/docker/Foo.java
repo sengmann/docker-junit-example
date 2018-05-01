03058bff8a424e7dd4d9d1f1878b133b94cda43e
Sascha Engmann <engmann@w11k.de>
2018-05-01 11:14:42 +0200

    init and running junit 5

---
+++ src/main/java/com/w11k/techday/docker/Foo.java
@@ -0,0 +1,13 @@
+package com.w11k.techday.docker;
+
+public class Foo {
+    private int counter = 0;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public int doUsefullStuff() {
+        return counter++;
+    }
+}
